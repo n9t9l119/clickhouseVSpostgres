90ad6e7284ed7bf979a0b109a54a7f1ff1a02282
n9t9l119 <n9t9l119@gmail.com>
2020-07-06 18:17:31 +0300

    Added functions for inserting CSV-data to databases. Problem: all data types in CSV file are string.

--- create_db.py
+++ create_db.py
@@ -1,7 +1,8 @@
 import psycopg2
 from clickhouse_driver import Client
-from tuple import create_tuple
 
+from tuple import create_tuple
+from csv_tuple import create_csv_tuple
 from postgres_creation import post
 from clickhouse_creation import click
 
@@ -11,11 +12,11 @@ pg_cursor = pg_con.cursor()
 
 ch_cursor = Client(host='localhost')
 
-# ch_cursor.execute('''DROP TABLE benchmark''')
+ch_cursor.execute('''DROP TABLE benchmark''')
 # pg_cursor.execute('''DROP TABLE benchmark''')
 
-tuple_lst = create_tuple()
+# tuple_lst = create_tuple()
+csv_tuple_lst = create_csv_tuple()
 
-
-click(ch_cursor, tuple_lst)
-post(pg_cursor, tuple_lst)
+click(ch_cursor, csv_tuple_lst)
+# post(pg_cursor, tuple_lst)
--- csv_tuple.py
+++ csv_tuple.py
@@ -1,10 +1,21 @@
 import csv
 
-
 def create_csv_tuple():
     with open('On_Time_Reporting_Carrier_On_Time_Performance_(1987_present)_2017_6.csv') as csv_file:
         csv_table = csv.reader(csv_file)
         csv_tuple = list(csv_table)
+        print (csv_table)
         # for row in csv_tuple:
         #     print(row)
         return csv_tuple
+
+
+def test_data_types():
+    with open('On_Time_Reporting_Carrier_On_Time_Performance_(1987_present)_2017_6.csv') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row)
+            for i in row:
+                print(type(i))
+
+
