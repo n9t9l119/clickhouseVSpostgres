998cc099bc08a3a63ad098f0f956d29757eb5d36
Anton <anton222000@gmail.com>
2020-07-09 16:02:13 +0300

    added try-except

--- databases_creation/deploy_databases.py
+++ databases_creation/deploy_databases.py
@@ -4,33 +4,38 @@ from postgres_creation import Postges
 from clickhouse_creation import ClickHouse
 
 
+try:
+    ch = ClickHouse('localhost')
+    pg = Postges('localhost', 'postgres', 'postgres')
 
-ch = ClickHouse('localhost')
-pg = Postges('localhost', 'postgres', 'postgres')
+    # start_time = time.time()
+    # dataset = dataset_reader.read_dataset()
+    # print("create csv tuple v1: %s sec. " % (time.time() - start_time))
 
-# start_time = time.time()
-# dataset = dataset_reader.read_dataset()
-# print("create csv tuple v1: %s sec. " % (time.time() - start_time))
+    # start_time = time.time()
+    # dataset = dataset_reader.read_dataset2()
+    # print("create csv tuple v2: %s sec. " % (time.time() - start_time))
 
-# start_time = time.time()
-# dataset = dataset_reader.read_dataset2()
-# print("create csv tuple v2: %s sec. " % (time.time() - start_time))
+    start_time = time.time()
+    dataset = dataset_reader.read_dataset3()
+    print("create csv tuple v3: %s sec. " % (time.time() - start_time))
 
-start_time = time.time()
-dataset = dataset_reader.read_dataset3()
-print("create csv tuple v3: %s sec. " % (time.time() - start_time))
+    start_time = time.time()
+    ch.create_and_insert(dataset)
+    print("insert ch: %s sec. " % (time.time() - start_time))
 
-start_time = time.time()
-ch.create_and_insert(dataset)
-print("insert ch: %s sec. " % (time.time() - start_time))
+    start_time = time.time()
+    pg.create_and_insert(dataset)
+    print("insert pg: %s sec. " % (time.time() - start_time))
 
-start_time = time.time()
-pg.create_and_insert(dataset)
-print("insert pg: %s sec. " % (time.time() - start_time))
+    # start_time = time.time()
+    # pg.create_and_insert2(dataset)
+    # print("insert pg: %s sec. " % (time.time() - start_time))
 
-# start_time = time.time()
-# pg.create_and_insert2(dataset)
-# print("insert pg: %s sec. " % (time.time() - start_time))
+    ch.close_connect()
+    pg.close_connect()
 
-ch.close_connect()
-pg.close_connect()
+except Exception as exc:
+    print(exc)
+
+input()
